33b4e75988b19151c206764e7ef1a3cbe3febca5
Cristina Agape <cristinamaria1998@yahoo.com>
2019-05-04 12:57:49 +0300

    4 buttons page

---
+++ android/app/src/main/kotlin/ro/cristina/gate_test/MainActivity.kt
@@ -0,0 +1,13 @@
+package ro.cristina.gate_test
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
